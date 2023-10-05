b903e69bf5a1cc3b8d96ac04de308e59192d16b0
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-05 13:53:43 +0400

    Practice1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/PersonalInfoPractice.py
@@ -0,0 +1,29 @@
+#personal information
+name = input ("Enter your name: ")
+print ("Hello!", name, "nice to meet you.")
+age = input ("Enter your age: ")
+print (age)
+birthday = input ("Enter your birthday: ")
+print (birthday)
+print ("Thank you for your time! But can you stay back and fill up the following?")
+
+#family
+fathername = input ("Enter your father's name: ")
+mothername = input ("Enter your mother's name: ")
+siblingname = input ("Enter your sibling/s' name: ")
+print ("Awww! Very nice!", fathername, mothername, siblingname)\
+
+print ("Wait! There's more...how about your educational background?")
+
+#education
+school = input ("Are you in school or university? Place 1 for school and 2 for university: ")
+name = input ("Where are you studying?: ")
+print (name, \
+       "...that's interesting, how about YOU?! Tell me more about yourself.")
+
+#your favorite things
+food = input ("Enter your favorite food: ")
+drink = input ("Enter your favorite drink: ")
+print (food, drink, "...tasty! I'm getting hungry just thinking about it.")
+movie = input ("Enter your favorite movie: ")
+print (movie, ", ...wow, okay, you and I should watch that some time.")
--- Chapter 1- Getting Started with Python/Exercises/Practice1.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice1.py
@@ -1,5 +1,5 @@
 #Q1: write a program to output the letters W X Y Z, each on a separate line
-print ("W, \
+print ("""W, \
        X, \
        Y, \
-       Z")
+       Z""")
