680c934bc0f41cafde5566289b9693ba26a09791
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-09 10:15:04 +0400

    Practice1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise.py/Exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise.py/Exercise4.py
@@ -3,3 +3,8 @@ middlename = "Alexia"
 lastname = "Cruz"
 name = firstname + middlename + lastname
 print (name)
+
+print ("hi \n how are you \n im good thank you")
+print ("wassup wassup \t what is this \t im just trying lolz")
+print ("up where they walk \n up where they run \n up where they stay all day in the sun")
+print ("out in the sea \t wish i could be \t part of your wooorrlllllldddd")
--- Chapter 1- Getting Started with Python/Exercises/Practice.py/Practice1.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice.py/Practice1.py
@@ -1,5 +1,5 @@
 #Q1: write a program to output the letters W X Y Z, each on a separate line
-print ("""W, \
-       X, \
-       Y, \
+print ("""W, \n
+       X, \n
+       Y, \n
        Z""")
