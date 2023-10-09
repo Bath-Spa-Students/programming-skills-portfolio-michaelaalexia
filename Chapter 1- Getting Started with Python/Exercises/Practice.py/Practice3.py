a32d1042f77cdb07d93e7dd59d83e3f526c643f0
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-09 09:30:43 +0400

    Practice3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice.py/Practice3.py
@@ -0,0 +1,10 @@
+#Q3: you need to make a program for a leader board. The program needs to output the numbers 1 to 9, each on a separate line followed by a dot
+print ("""1.
+       2.
+       3.
+       4.
+       5.
+       6.
+       7.
+       8.
+       9.""")
