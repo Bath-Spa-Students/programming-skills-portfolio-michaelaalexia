bdc0f216549514343b22c5cedf38d709c928405a
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-09 09:31:58 +0400

    Practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice.py/Practice5.py
@@ -0,0 +1,6 @@
+#Q5: write four strings in different variables and print the output as one string
+firstname = "Michaela"
+secondname = "Alexia"
+middlename = "Salisi"
+lastname = "Cruz"
+print (firstname, secondname, middlename, lastname)
