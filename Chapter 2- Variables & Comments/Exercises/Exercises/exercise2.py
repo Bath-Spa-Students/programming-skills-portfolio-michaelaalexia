255ab82074d990b788157e20125e7dbeb5ff66c8
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-12 13:29:27 +0400

    exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercises/exercise2.py
@@ -0,0 +1,2 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+print ("Obi Wan Kenobi once said," , """ "Your eyes can deceive you, do not trust them." """)
