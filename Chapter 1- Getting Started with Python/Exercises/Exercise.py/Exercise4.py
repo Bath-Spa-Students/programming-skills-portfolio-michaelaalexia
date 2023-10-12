c94492a501deda20c61a548170697b3484bf4395
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-12 13:23:48 +0400

    exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise.py/Exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise.py/Exercise4.py
@@ -4,7 +4,3 @@ lastname = "Cruz"
 name = firstname + middlename + lastname
 print (name)
 
-print ("hi \n how are you \n im good thank you")
-print ("wassup wassup \t what is this \t im just trying lolz")
-print ("up where they walk \n up where they run \n up where they stay all day in the sun")
-print ("out in the sea \t wish i could be \t part of your wooorrlllllldddd")
---
+++ Chapter 2- Variables & Comments/Exercises/Exercises/exercise1.py
@@ -0,0 +1,5 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+greeting = "good morning beautiful people."
+print (greeting)
+greeting = "hello there."
+print (greeting)
