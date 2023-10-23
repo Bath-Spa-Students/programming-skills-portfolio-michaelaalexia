02d69a646aa9910094def6f84a26b4999119ccd3
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-23 09:49:05 +0400

    exercise5.py

---
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -0,0 +1,11 @@
+#You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+#Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
+#Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+#Print a second set of invitation messages, one for each person who is still in your list.
+invitees = ["JoJo", "LaLa", "YanYan", "DanDan", "TanTan"]
+invitees[4] = "VenVen"
+print ("Hey! ", invitees[0], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[1], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[2], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[3], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[4], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
