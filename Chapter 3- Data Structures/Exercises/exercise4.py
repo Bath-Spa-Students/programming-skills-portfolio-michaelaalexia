3096a200d1ae4d6d722300efa3eb6e1c328c60ba
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-23 09:44:14 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner
+invitees = ["JoJo", "LaLa", "YanYan", "DanDan", "TanTan"]
+print ("Hey! ", invitees[0], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[1], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[2], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[3], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
+print ("Hey! ", invitees[4], " wanna come with me to dinner? My place at 7? Let me know when you can come or not!")
