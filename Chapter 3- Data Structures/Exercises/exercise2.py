e862f6ff3c93a705c84f9482df19ec063201d6ca
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-10-23 09:27:18 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,17 @@
+#Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+names = ["Micah", "Joanna", "Angela", "Aryan", "Abed", "Jordan", "Daven"]
+print (names)
+Micah = "Micah : the tired mom of the group" 
+Joanna = "Joanna : the diva whose only vocabulary is 'slay' and 'yass queen'"
+Angela = "Angela : the energetic history nerd"
+Aryan = "Aryan : the one who captures every moment"
+Abed = "Abed : the lost sheep who was never really lost"
+Jordan = "Jordan : the crazy chef"
+Daven = "Daven : the guy who doesn't read"
+print (Micah)
+print (Joanna)
+print (Angela)
+print (Aryan)
+print (Abed)
+print (Jordan)
+print (Daven)
