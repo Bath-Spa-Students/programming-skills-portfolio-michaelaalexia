a89b777a3f0ec91b4ff5043a21941cf552051d59
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-11-07 17:16:34 +0400

    exercise5.py

--- Chapter 6- Loops/Exercises/exercise5.py
+++ Chapter 6- Loops/Exercises/exercise5.py
@@ -2,10 +2,10 @@
 #Add code near the beginning of your program to print a message saying the deli has run out of pastrami, 
 #and then use a while loop to remove all occurrences of 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up in finished_sandwiches.
 
-sandwich_orders = ["A Sandwich", "B Sandwich", "C Sandwich", "Pastrami", "Pastrami", "Pastrami"]  
+sandwich_orders = ["Chicken Sandwich", "Tuna Sandwich", "Cheese Sandwich", "Pastrami", "Pastrami", "Pastrami"]  
 finished_sandwiches = []
 
-print (sandwich_orders)
+
 print ("Sorry, the Deli has run out of pastrami...")
 while "Pastrami" in sandwich_orders:
     sandwich_orders.remove("Pastrami")
