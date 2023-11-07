85521c20ec29f239c9d6f2a3f6a7ee72a4699816
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-11-07 17:15:43 +0400

    exercise4.py

--- Chapter 6- Loops/Exercises/exercise4.py
+++ Chapter 6- Loops/Exercises/exercise4.py
@@ -2,7 +2,7 @@
 #Loop through the list of sandwich orders and print a message for each order, such as I made your tuna sandwich. As each sandwich is made, 
 #move it to the list of finished sandwiches. After all the sandwiches have been made, print a message listing each sandwich that was made.
 
-sandwich_orders = ["A Sandwich", "B Sandwich", "C Sandwich"]
+sandwich_orders = ["Tuna Sandwich", "Chicken Sandwich", "Cheese Sandwich"]
 finished_sandwiches = []
 
 for x in sandwich_orders:
