b2c0dd7df30c28e8be1c222a2abe654c3ae7f5ea
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-11-08 15:15:47 +0400

    exercise1.py

--- Chapter 6- Loops/Exercises/exercise1.py
+++ Chapter 6- Loops/Exercises/exercise1.py
@@ -1,8 +1,5 @@
-## Exercise 1: Pizza Toppings :ballot_box_with_check:
-
-##Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping,
-
-##print a message saying you’ll add that topping to their pizza.
+#Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value. As they enter each topping,
+#print a message saying you’ll add that topping to their pizza.
 
 expected_response = "Enough"
 response = input("Enter Pizza Topping: ")
