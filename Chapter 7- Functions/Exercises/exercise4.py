e862cbf1a0d33cd856889ee13be95c7cc0081ec1
michaelaalexia <144330215+michaelaalexia@users.noreply.github.com>
2023-11-10 19:18:03 +0400

    exercise4.py

--- Chapter 7- Functions/Exercises/exercise4.py
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -1,7 +1,9 @@
 #Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. 
 #Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 
-def make_shirt(size, message):
-    print ("The size is: ", size, " with this as the message: ", message)
+def make_shirt(size = "L", message = "I LOVE PYTHON!"):
+    print ("The size is: ", size, " with the message: ", message)
 
-make_shirt("L", "I LOVE PYTHON!")
+make_shirt()
+make_shirt("M", "I LOVE PYTHON!")
+make_shirt("S", "Hello World!")
